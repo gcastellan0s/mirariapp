9b65aa0d92c8574b3fe336b1ef9e59ffd81a7cfe
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2018-10-29 16:29:43 -0600

    Activando Celery y Enviados a en notificaciones

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
     'localflavor',
     'django_countries',
     'corsheaders',
+    'django_celery_results',
 ]
 LOCAL_APPS = [
     'mirari.mirari',
@@ -244,7 +245,7 @@ if USE_TZ:
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
 CELERY_BROKER_URL = env('CELERY_BROKER_URL')
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
-CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_RESULT_BACKEND = 'django-db'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
 CELERY_ACCEPT_CONTENT = ['json']
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
