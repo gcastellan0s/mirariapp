312f4addea1e818accf967c041a90a6b027c3dce
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-02-18 16:37:04 -0600

    Tablas de amortizacion y Sellpoint

--- mirariapp/src/renderer/app/mirari/api/session.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const session = axios.create({
-    xsrfCookieName: 'csrftoken',
-    xsrfHeaderName: 'X-CSRFToken',
-    baseURL: '//'+require('@/../../package.json').api,
-});
-
-export default session;
