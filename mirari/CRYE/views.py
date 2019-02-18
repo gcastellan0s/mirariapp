4c45c8de1632ca9a06af56be81462be4e162110c
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-02-18 16:51:24 -0600

    Tablas de Amortizacion y punto de venta

--- mirari/CRYE/views.py
+++ mirari/CRYE/views.py
@@ -29,3 +29,35 @@ class SiebelUnblock__SiebelUnblock__TemplateView(Generic__TemplateView):
 	def proccess_context(self, context):
 		context['object'] = self.model
 		return context
+	
+class TablaAmortizacion__TemplateView(Generic__TemplateView):
+	model = apps.get_model(APP, 'TablaAmortizacion')
+	template_name = "TablaAmortizacion__TemplateView.html"
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		message, api = 'Hay un error en tu consulta', 'error' 
+		if request.method == 'POST':
+			if request.GET.get('api') == 'getTablaAmortizacion':
+				#try:
+				import cx_Oracle
+				db = DBConnection.objects.filter(name='siebel', organization__pk=self.request.session.get('organization')).first()
+				con = cx_Oracle.connect(db.db_name+'/'+db.db_password+'@'+db.db_host+'/'+db.db_user, encoding = "UTF-8", nencoding = "UTF-8")
+				con.autocommit = True
+				cursor = con.cursor()
+
+				#query = "select ACCUM_VAL NUM_PAGO,to_char(END_DT,'DD/MM/YYYY') FECHA,X_ESTATUS_FACTURA STATUS,END_BALANCE INSOLUTO,CASH_SURRENDER_VAL CAPITAL,INTEREST_PAID INTERESES,FEE_PAID RENTA, HIGH_BALANCE PAGADO from S_FN_ACCNT_BAL where X_CLAVE_PH like '3-DKRL9%' order by ACCUM_VAL"
+				query = "SELECT ref_number_3,owner_accnt_id,PR_CON_ID FROM siebline.S_ASSET WHERE asset_num='{0}'".format(request.POST.get('number'))
+				#query = """update siebline.S_ASSET set status_cd='Análisis' where asset_num='{0}'""".format(request.POST.get('number'))
+				print(query)
+
+				cursor.execute(query)
+				cursor.close()
+				message, api = 'Solicitud atendida', 'success' 
+				#except Exception as e:
+					#message, api = str(e), 'error' 
+			return JsonResponse({'message':message,'api':api})
+		return super().dispatch(request, *args, **kwargs)
+	###############################################################################################
+	def proccess_context(self, context):
+		context['object'] = self.model
+		return context
