f3a2a185e27e74bf8980aa1192d0a4b00d05e6eb
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-03-03 15:30:21 -0600

    Actualizando bases de TCS

--- mirari/SV/nodejs/Socketio__SV.js
+++ mirari/SV/nodejs/Socketio__SV.js
@@ -3,28 +3,10 @@ io.on('connection', function (socket) {
     socket.on('room', function(data) { 
         socket.join(data); 
     })
-    socket.on('Send_EmmitTicket', function (data) {
-        io.sockets.in(data.company).emit('Recibe_EmmitTicket', data);
+    //socket.on('Send_EmmitTicket', function (organizationId, data) {
+        //io.sockets.in(organizationId).emit('Recibe_EmmitTicket', data);
+    //});
+    socket.on('Send_PrintTicket', function (organizationId, data) {
+        io.sockets.in(organizationId).emit('Recibe_PrintTicket', data);
     });
-    socket.on('Send_PrintTicket', function (data) {
-        io.sockets.in(data.company).emit('Recibe_PrintTicket', data);
-    });
-    //socket.on('payment_ticket', function (data) {
-        //io.sockets.in(data.company).emit('payment_ticket', data);
-    //});
-    //socket.on('print_cut', function (data) {
-        //io.sockets.in(data.company).emit('cordova_cut', data);
-    //});
-    //socket.on('payment_ticket', function (data) {
-        //io.sockets.in(data.company).emit('payment_ticket', data);
-    //});
-    //socket.on('reload', function (data) {
-        //io.sockets.in(data.company).emit('reload');
-    //});
-    //socket.on('update_products', function (data) {
-        //io.sockets.in(data.company).emit('update_products');
-    //});
-    //socket.on('printer_connected', function (data) {
-        //io.sockets.in(data.company).emit('printer_connected', data);
-    //});
 });
