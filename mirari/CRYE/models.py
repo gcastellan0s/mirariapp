e48a5a0c351b18db54c8433c02fbb1e32a08cc32
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-03-07 12:09:00 -0600

    Tabla de amortizacion

--- mirari/CRYE/models.py
+++ mirari/CRYE/models.py
@@ -136,7 +136,7 @@ class WalletCredit(Model_base):
 			return '$ {0}'.format(self.monto)
 		return '-'
 	def url_add(self):
-		return reverse('CRYE:WalletCredit__TemplateView', kwargs={'model': self.VARS['MODEL']})
+		return reverse('CRYE:WalletCredit__TemplateView', kwargs={'app': self.VARS['APP'], 'model': self.VARS['MODEL']})
 	def url_update(self):
 		return reverse('CRYE:TablaAmortizacion__TemplateView', kwargs={'app': self.VARS['APP'], 'model': self.VARS['MODEL'], 'pk': self.pk})
 
