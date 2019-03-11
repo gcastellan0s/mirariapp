f39dce3f4079c7a191cea004792bbb2da173433f
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-03-11 17:37:05 -0600

    Cambios

--- mirari/SV/printer/print.js
+++ mirari/SV/printer/print.js
@@ -1,7 +1,6 @@
 'use strict';
 const variables = require('./variables');
 const {PythonShell} = require("python-shell");
-const io = require('socket.io')
 const ip = require('ip');
 
 var PublicSocket = require('socket.io-client')('ws://50.18.229.242:8002');
@@ -11,10 +10,11 @@ var data = {
 	url: 'ws://'+ip.address().toString()+':'+variables.port.toString(),
 	name: variables.sellpoint,
 	organizationCode: variables.organizationCode,
+	isServer: variables.isServer
 }
 
 let PrinterConection = () => {
-	PublicSocket.emit('Send_PrinterConection', variables.organizationCode, data);
+	PublicSocket.emit('Send_Event', 'PrinterConection', variables.organizationCode, data);
 };
 
 PrinterConection();
@@ -23,13 +23,15 @@ setInterval(function(){
 }, 15000);
 
 var io = require('socket.io')(variables.port);
+
 io.on('connection', function (socket) {
-	socket.on('Recibe_PrintTicket', function(data) {
-		var pyshell = new PythonShell('print_ticket.py', {
+	socket.on('Send_Print', function(event, data) {
+		if (event == 'PrintTicket') script = 'printTicket.py';
+		if (event == 'PrintCut') script = 'printCut.py';
+		var pyshell = new PythonShell(script, {
 			mode: 'json',
 			pythonPath: '/usr/bin/python3',
-			scriptPath: '/home/pi/mirari-printer',
-			isServer: variables.isServer
+			scriptPath: '/home/pi/printer',
 		});
 		pyshell.send(data).end(function (err) {
 			if (err){
@@ -38,24 +40,7 @@ io.on('connection', function (socket) {
 			}
 		});
 	});
-	socket.on('Recibe_MakeCut', function (data) {
-		var pyshell = new PythonShell('make_cut.py', {
-			mode: 'json',
-			pythonPath: '/usr/bin/python3',
-			scriptPath: '/home/pi/mirari-printer',
-		});
-		pyshell.send(data).end(function (err) {
-			if (err) {
-				console.log(err)
-				return 0;
-			}
-		});
+	socket.on('Send_Event', function (organizationCode, event, data) {
+		io.sockets.in(organizationCode).emit('Recibe_Event', event, data);
 	});
-	socket.on('Send_TicketPayment', function (organizationCode, data) {
-		io.sockets.in(organizationCode).emit('Recibe_TicketPayment', data);
-	});
-	socket.on('Send_TicketPayment', function (organizationCode, data) {
-		io.sockets.in(organizationCode).emit('Recibe_TicketPayment', data);
-	});
-	
 });
