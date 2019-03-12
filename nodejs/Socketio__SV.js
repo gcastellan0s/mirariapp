1dc2c3fd0abe7c291b3c2a4f7f470d718208ca76
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-03-11 18:01:01 -0600

    Cambios

--- nodejs/Socketio__SV.js
+++ nodejs/Socketio__SV.js
@@ -4,7 +4,6 @@ io.on('connection', function (socket) {
         socket.join(data); 
     })
     socket.on('Send_Event', function (organizationCode, event, data) {
-        console.log(event)
         io.sockets.in(organizationCode).emit('Recibe_Event', event, data);
     });
 });
