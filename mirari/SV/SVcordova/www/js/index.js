a88fd3d89d659fab8ec54081e2bc1363baf12bae
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-03-19 00:11:58 -0600

    SV Formato de tickets

--- mirari/SV/SVcordova/www/js/index.js
+++ mirari/SV/SVcordova/www/js/index.js
@@ -3,7 +3,6 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
     onDeviceReady: function() {
-        cordova.plugins.backgroundMode.enable();
         var socket = io('ws://50.18.229.242:8002')
         socket.emit('room', 'tahona');
         var ref = cordova.InAppBrowser.open('http://tahona.tahona.mx/', '_blank', 'location=no,toolbar=no,zoom=no,presentationstyle=fullscreen');
