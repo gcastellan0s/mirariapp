9d7e303dbef601adc0035dcceab2bae610599217
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-07-26 01:07:30 -0500

    Actualizando migración de MXF

--- mirari/TCS/migration.py
+++ mirari/TCS/migration.py
@@ -171,3 +171,38 @@ with open('temp/mexicof/ordenes_concepto.csv') as csv_file:
             orderServiceConcept.creation_date = dateutil.parser.parse(row[3])
             orderServiceConcept.id_bckp = row[0]
             orderServiceConcept.save()
+
+with open('temp/mexicof/ordenes_concepto.csv') as csv_file: 
+    csv_reader = csv.reader(csv_file, delimiter=',') 
+    for row in csv_reader: 
+        orderServiceConcept = OrderServiceConcept.objects.filter(id_bckp=row[0], orderservice__organization=o).first()
+        if not orderServiceConcept:
+            orderServiceConcept = OrderServiceConcept()
+            orderServiceConcept.orderservice = OrderService.objects.filter(id_bckp=row[5], organization=o).first()
+            if row[6]:
+                orderServiceConcept.user = User.objects.filter(id_bckp=row[6], organization=o).first()
+            else:
+                orderServiceConcept.user = None
+            orderServiceConcept.concept = row[1]
+            orderServiceConcept.quantity = row[2]
+            orderServiceConcept.creation_date = dateutil.parser.parse(row[3])
+            orderServiceConcept.id_bckp = row[0]
+            orderServiceConcept.save()
+    
+with open('temp/mexicof/ordenes_mensaje.csv') as csv_file: 
+    csv_reader = csv.reader(csv_file, delimiter=',') 
+    for row in csv_reader: 
+        orderServiceComment = OrderServiceComment.objects.filter(id_bckp=row[0], orderservice__organization=o).first()
+        if not orderServiceComment:
+            orderServiceComment = OrderServiceComment()
+            orderServiceComment.orderservice = OrderService.objects.filter(id_bckp=row[3], organization=o).first()
+            if row[4]:
+                orderServiceComment.user = User.objects.filter(id_bckp=row[4], organization=o).first()
+            else:
+                orderServiceComment.user = UNone
+            orderServiceComment.comment = row[2]
+            orderServiceComment.creation_date = dateutil.parser.parse(row[1])
+            orderServiceComment.id_bckp = row[0]
+            orderServiceComment.save()
+            
+            
