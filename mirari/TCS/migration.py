22825713742019fd4ed28fe7a48db0a4d5b25fdf
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-07-28 21:53:45 -0500

    Cambiando TCS

--- mirari/TCS/migration.py
+++ mirari/TCS/migration.py
@@ -93,71 +93,79 @@ with open('temp/tecnoservicio/ordenes_orden.csv') as csv_file:
     for row in csv_reader: 
         orderService = OrderService.objects.filter(id_bckp=row[0], organization=o).first()
         if not orderService:
-            #try:
             orderService = OrderService()
-            orderService.serial = row[1]
-            orderService.organization = o
-            orderService.creation_date = dateutil.parser.parse(row[2])# datetime.datetime.strptime(row[2].replace('/','-').split('+')[0], '%Y-%m-%d %H:%M:%S.%f')
-            if row[34]:
-                orderService.user = User.objects.filter(id_bckp=row[34], organization=o).first()
-            else:
-                orderService.user = None
-            if row[35]:
-                orderService.technical = User.objects.filter(id_bckp=row[35], organization=o).first()
-            else:
-                orderService.technical = None
-            orderService.status = row[3]
-            orderService.service = row[4]
-            orderService.zone = row[5]
-            orderService.concept = row[6]
-            if row[7]:
-                orderService.service_date = dateutil.parser.parse(row[7]) #datetime.datetime.strptime(row[7], '%Y-%m-%d')
-            else:
-                orderService.service_date = None
-            if row[8]:
-                orderService.buy_date = dateutil.parser.parse(row[8]) #datetime.datetime.strptime(row[8], '%Y-%m-%d')
-            else:
-                orderService.buy_date = None
-            if row[9]:
-                orderService.delivery_date = dateutil.parser.parse(row[9]) #datetime.datetime.strptime(row[9], '%Y-%m-%d')
-            else:
-                orderService.delivery_date = None
-            orderService.client_name = row[10]
-            orderService.email = row[11]
-            orderService.contact_phone1 = row[12]
-            orderService.contact_phone2 = row[13]
-            orderService.contact_phone3 = row[14]
-            orderService.address = row[15]
-            orderService.colony = row[16]
-            orderService.city = row[17]
-            orderService.cp = row[18]
-            orderService.address_reference = row[19] + ' ' + row[20]
-            orderService.client_notes = row[23]
-            orderService.company = Company.objects.get(id_bckp=row[31], organization=o)
-            orderService.companyName = Company.objects.get(id_bckp=row[31], organization=o).name
-            orderService.store = Store.objects.get(id_bckp=row[36], company__organization=o)
-            orderService.storeName = Store.objects.get(id_bckp=row[36], company__organization=o).name
-            orderService.brand = Brand.objects.get(id_bckp=row[32], organization=o)
-            orderService.brandName = Brand.objects.get(id_bckp=row[32], organization=o).name
-            orderService.report_name = row[21]
-            if row[33]:
-                orderService.modelo = Modelo.objects.get(id_bckp=row[33], brand__organization=o)
-                orderService.modeloName = Modelo.objects.get(id_bckp=row[33], brand__organization=o).name
-            else:
-                orderService.modelo = None
-                orderService.modeloName = None
-            orderService.serial_number = row[22]
-            orderService.hidden_notes = row[23]
-            orderService.order_notes = row[24]
-            orderService.icon_os = row[25]
-            orderService.icon_ics = row[26]
-            orderService.icon_ics_2 = row[27]
-            orderService.icon_ics_3 = row[28]
-            orderService.icon_on = row[29]
-            orderService.icon_cn = row[30]
-            orderService.id_bckp = row[0]
-            orderService.comments = row[24]
-            orderService.save()
+        orderService.serial = row[1]
+        orderService.organization = o
+        orderService.creation_date = dateutil.parser.parse(row[2])# datetime.datetime.strptime(row[2].replace('/','-').split('+')[0], '%Y-%m-%d %H:%M:%S.%f')
+        if row[34]:
+            orderService.user = User.objects.filter(id_bckp=row[34], organization=o).first()
+        else:
+            orderService.user = None
+        if row[35]:
+            orderService.technical = User.objects.filter(id_bckp=row[35], organization=o).first()
+        else:
+            orderService.technical = None
+        orderService.status = row[3]
+        orderService.service = row[4]
+        orderService.zone = row[5]
+        orderService.concept = row[6]
+        if row[7]:
+            try:
+                orderService.service_date = datetime.datetime.strptime(row[7], '%Y-%m-%d')
+            except:
+                orderService.service_date = dateutil.parser.parse(row[7])
+        else:
+            orderService.service_date = None
+        if row[8]:
+            try:
+                orderService.service_date = datetime.datetime.strptime(row[8], '%Y-%m-%d')
+            except:
+                orderService.service_date = dateutil.parser.parse(row[8])
+        else:
+            orderService.buy_date = None
+        if row[9]:
+            try:
+                orderService.service_date = datetime.datetime.strptime(row[9], '%Y-%m-%d')
+            except:
+                orderService.service_date = dateutil.parser.parse(row[9])
+        else:
+            orderService.delivery_date = None
+        orderService.client_name = row[10]
+        orderService.email = row[11]
+        orderService.contact_phone1 = row[12]
+        orderService.contact_phone2 = row[13]
+        orderService.contact_phone3 = row[14]
+        orderService.address = row[15]
+        orderService.colony = row[16]
+        orderService.city = row[17]
+        orderService.cp = row[18]
+        orderService.address_reference = row[19] + ' ' + row[20]
+        orderService.client_notes = row[23]
+        orderService.company = Company.objects.get(id_bckp=row[31], organization=o)
+        orderService.companyName = Company.objects.get(id_bckp=row[31], organization=o).name
+        orderService.store = Store.objects.get(id_bckp=row[36], company__organization=o)
+        orderService.storeName = Store.objects.get(id_bckp=row[36], company__organization=o).name
+        orderService.brand = Brand.objects.get(id_bckp=row[32], organization=o)
+        orderService.brandName = Brand.objects.get(id_bckp=row[32], organization=o).name
+        orderService.report_name = row[21]
+        if row[33]:
+            orderService.modelo = Modelo.objects.get(id_bckp=row[33], brand__organization=o)
+            orderService.modeloName = Modelo.objects.get(id_bckp=row[33], brand__organization=o).name
+        else:
+            orderService.modelo = None
+            orderService.modeloName = None
+        orderService.serial_number = row[22]
+        orderService.hidden_notes = row[23]
+        orderService.order_notes = row[24]
+        orderService.icon_os = row[25]
+        orderService.icon_ics = row[26]
+        orderService.icon_ics_2 = row[27]
+        orderService.icon_ics_3 = row[28]
+        orderService.icon_on = row[29]
+        orderService.icon_cn = row[30]
+        orderService.id_bckp = row[0]
+        orderService.comments = row[24]
+        orderService.save()
             #except Exception as e:
                 #print(row[1], str(e))
             
