99265f7c4d81c51645861b7a6627f89872b42b0c
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-07-30 10:25:10 -0500

    Cambiando TCS

--- config/settings/base.py
+++ config/settings/base.py
@@ -249,29 +249,29 @@ ADMINS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
-# Celery
-# ------------------------------------------------------------------------------
-INSTALLED_APPS += ['mirari.taskapp.celery.CeleryAppConfig']
-
-if USE_TZ:
+## Celery
+## ------------------------------------------------------------------------------
+#INSTALLED_APPS += ['mirari.taskapp.celery.CeleryAppConfig']
+#
+#if USE_TZ:
     # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-timezone
-    CELERY_TIMEZONE = TIME_ZONE
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
-CELERY_BROKER_URL = env('CELERY_BROKER_URL')
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
-CELERY_RESULT_BACKEND = CELERY_BROKER_URL
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
-CELERY_ACCEPT_CONTENT = ['json']
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
-CELERY_TASK_SERIALIZER = 'json'
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_serializer
-CELERY_RESULT_SERIALIZER = 'json'
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-time-limit
-# TODO: set to whatever value is adequate in your circumstances
-CELERYD_TASK_TIME_LIMIT = 5 * 60
-# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
-# TODO: set to whatever value is adequate in your circumstances
-CELERYD_TASK_SOFT_TIME_LIMIT = 60
+    #CELERY_TIMEZONE = TIME_ZONE
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
+#CELERY_BROKER_URL = env('CELERY_BROKER_URL')
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
+#CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
+#CELERY_ACCEPT_CONTENT = ['json']
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
+#CELERY_TASK_SERIALIZER = 'json'
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_serializer
+#CELERY_RESULT_SERIALIZER = 'json'
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-time-limit
+## TODO: set to whatever value is adequate in your circumstances
+#CELERYD_TASK_TIME_LIMIT = 5 * 60
+## http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
+## TODO: set to whatever value is adequate in your circumstances
+#CELERYD_TASK_SOFT_TIME_LIMIT = 60
 
 
 ## django-allauth
