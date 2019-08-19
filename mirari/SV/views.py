a92f95abb19f8fd4feda75ae38885eb80e6cdd47
Gustavo Castellanos <g@gustavo-castellanos.com>
2019-08-18 21:26:19 -0500

    Inventarios SV

--- mirari/SV/views.py
+++ mirari/SV/views.py
@@ -81,8 +81,8 @@ class Sellpoint__ApiView(Generic__ApiView):
 			return JsonResponse({'ProductAttributes':ProductAttributesSerializer(productAttributes, read_only=True).data}, safe=False)
 		elif Action == 'makeCut':
 			#return JsonResponse({'cut': CutSerializer(Cut.objects.get(id=2691)).data}, safe=False)
-			#if not request.POST.get('cutID') == 'Actual':
-				#return JsonResponse({'cut': CutSerializer(Cut.objects.get(id=request.POST.get('cutID'))).data}, safe=False)
+			if not request.POST.get('cutID') == 'Actual':
+				return JsonResponse({'cut': CutSerializer(Cut.objects.get(id=request.POST.get('cutID'))).data}, safe=False)
 			cut = Sellpoint.objects.get(id=json.loads(request.POST.get('sellpoint'))['id']).getCut()
 			cut = cut.makeCut()
 			if cut:
