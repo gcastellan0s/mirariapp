e0048084cf2a4295e6eeb354ebf8381b1caf1310
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-08-27 15:23:32 -0500

    Cambio ORACLE

--- mirari/CRYE/views.py
+++ mirari/CRYE/views.py
@@ -284,6 +284,7 @@ class TablaAmortizacion2__TemplateView(Generic__TemplateView):
         return super().dispatch(request, *args, **kwargs)
     ###########################################################################################
     def proccess_context(self, context):
+        import os 
         context['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH']
         context['ORACLE_HOME'] = os.environ['ORACLE_HOME']
         context['LD_RUN_PATH'] = os.environ['LD_RUN_PATH']
