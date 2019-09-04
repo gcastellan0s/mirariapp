da477e3adf4b82723b8449ea91dbddaf76fd3c74
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-09-04 13:33:37 -0500

    Cambios

--- mirari/TCS/models.py
+++ mirari/TCS/models.py
@@ -621,10 +621,10 @@ class OrderService(Model_base):
     def QUERY(self, view):
         team_codes = view.request.user.get_groups()
         orderService = OrderService.objects.filter(organization__pk=view.request.session.get('organization'), active=True).distinct()
-        if 'FORANEO' in team_codes or 'LOCALES' in team_codes:
-            orderService = orderService.filter(technical=view.request.user).distinct()
+        #if 'FORANEO' in team_codes or 'LOCALES' in team_codes:
+            #returnorderService = orderService.filter(technical=view.request.user).distinct()
         if len(view.request.GET.get('search[value]', '')) > 3:
-            return orderService[0:50]
+            return orderService
         return orderService.filter(service_date__gt=datetime.datetime.now()-timedelta(days=30))
     def get_id_html(self):
         return '<strong class="mr-2 m--icon-font-size-lg3">{0}</strong> <small>[{1}]</small><br />'.format(self.id, self.service.upper())
