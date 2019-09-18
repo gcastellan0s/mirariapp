c5631831536222d6911df49ffe5ba71befe7bcc8
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-09-18 17:44:51 -0500

    Cambioss

--- mirari/SV/views.py
+++ mirari/SV/views.py
@@ -32,6 +32,7 @@ class Sellpoint__ApiView(Generic__ApiView):
 		elif Action == 'getReport':
 			sellpointgroup = SellpointGroups.objects.get(pk=request.POST.get('sellpointgroup'))
 			cut = Cut.objects.filter(sellpoint__in=sellpointgroup.sellpoints.all(), final_time__year=2019, final_time__month=5)
+			return JsonResponse({'cuts':len(cut)}, safe=False)
 			try:
 				return JsonResponse({'cuts':CutSerializer(cut, many=True).data}, safe=False)
 			except Exception as e:
