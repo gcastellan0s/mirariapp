f641b1f1f97a0e8b6d125b2ef7d1414a981dce7e
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-12-16 15:01:46 -0600

    Cambios

--- mirari/STR/models.py
+++ mirari/STR/models.py
@@ -81,14 +81,7 @@ VARS = {
     'FORM': [
         Div(
             Div('name', css_class="col-md-12"),
-            Div(
-                Div(
-                    Div('canBySell', css_class="col-md-12"),
-                    Div('canByBuy', css_class="col-md-12"),
-                    css_class="form-group m-form__group row"
-                ),
-                css_class="col-md-6"
-            ),
+            Div('canBySell', css_class="col-md-6"),
             css_class="form-group m-form__group row"
         ),
     ],
