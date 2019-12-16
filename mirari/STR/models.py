1941f49895fb1bd853f1302d81df3332fa3864ec
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-12-16 14:10:19 -0600

    Cambios

--- mirari/STR/models.py
+++ mirari/STR/models.py
@@ -77,7 +77,16 @@ VARS = {
             'title': 'NOMBRE',
         },
     ],
-    'FORM': ('name','canBySell','canByBuy','typeProduct','category','sellPrice','costPrice','notes','codebar','uid','minimumQuantity','maximumQuantity','deliveryTerm','weight','volume','users','deliveryDescription','receptionsDescription'),
+    'FORM_CLASS': 'small_form',
+    'FORM': [
+        Div(
+            Div('name', css_class="col-md-12"),
+            Div('canBySell', css_class="col-md-4"),
+            Div('canByBuy', css_class="col-md-4"),
+            css_class="form-group m-form__group row"
+        ),
+    ],
+    #'FORM': ('name','canBySell','canByBuy','typeProduct','category','sellPrice','costPrice','notes','codebar','uid','minimumQuantity','maximumQuantity','deliveryTerm','weight','volume','users','deliveryDescription','receptionsDescription'),
     #'SELECTQ': {
         #'technical': {
             #'model': ['STR', 'CategoryProduct'],
@@ -117,9 +126,9 @@ class Product(Model_base):
     weight = models.FloatField('Peso', blank=True, null=True)
     volume = models.FloatField('Volumen', blank=True, null=True)
     deliveryTerm = models.IntegerField('Plazo de entrega', blank=True, null=True)
-    users = models.ManyToManyField('mirari.User')
-    deliveryDescription = models.TextField(blank=True)
-    receptionsDescription = models.TextField(blank=True)
+    users = models.ManyToManyField('mirari.User', verbose_name="Responsables")
+    deliveryDescription = models.TextField(blank=True, verbose_name="Descripción para pedidos de entrega")
+    receptionsDescription = models.TextField(blank=True, verbose_name="Descripción para recepciones")
     
     minimumQuantity = models.IntegerField(blank=True, null=True)
     maximumQuantity = models.IntegerField(blank=True, null=True)
@@ -132,3 +141,23 @@ class Product(Model_base):
         permissions = permissions(VARS)
     def __str__(self):
         return self.name
+
+class ProductHistory(Model_base):
+    HISTORYTYPE = (
+        ('ENTRADA','ENTRADA'),
+        ('SALIDA','SALIDA'),
+        ('ACTUALIZACION','ACTUALIZACION'),
+    )
+    user = models.ForeignKey('mirari.User', blank=True, null=True)
+    historyType = models.CharField('Tipo de producto', choices=HISTORYTYPE, max_length=250, default="productQuantity")
+    datetime = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey('STR.Product', blank=True, null=True)
+    id_bckp = models.IntegerField(blank=True, null=True)
+    class Meta(Model_base.Meta):
+        verbose_name = VARS['NAME']
+        verbose_name_plural = VARS['PLURAL']
+        permissions = permissions(VARS)
+    def __str__(self):
+        return self.name
+    def QUERY(self, view):
+        return ProductHistory.filter(product__organization__pk=view.request.session.get('organization'))
