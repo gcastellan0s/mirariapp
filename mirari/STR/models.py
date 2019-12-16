c2b7a5d0ff8ca73c7733a6ef23b37933bc694bc9
Gustavo Castellanos <gustavocastellanos@Gustavos-MacBook-Pro.local>
2019-12-16 15:12:41 -0600

    Cambios

--- mirari/STR/models.py
+++ mirari/STR/models.py
@@ -85,6 +85,22 @@ VARS = {
         ),
         Div('canBySell', css_class="col-md-12"),
         Div('canByBuy', css_class="col-md-12"),
+        Div(
+            Div(
+                Div('typeProduct', css_class="col-md-12"),
+                Div('category', css_class="col-md-12"),
+                Div('uid', css_class="col-md-12"),
+                Div('codebar', css_class="col-md-12"),
+                css_class="col-md-6"
+            ),
+            Div(
+                Div('sellPrice', css_class="col-md-12"),
+                Div('costPrice', css_class="col-md-12"),
+                css_class="col-md-6"
+            ),
+            css_class="form-group m-form__group row"
+        ),
+        Div('notes', css_class="col-md-12"),
     ],
     #'FORM': ('name','canBySell','canByBuy','typeProduct','category','sellPrice','costPrice','notes','codebar','uid','minimumQuantity','maximumQuantity','deliveryTerm','weight','volume','users','deliveryDescription','receptionsDescription'),
     #'SELECTQ': {
