e5463c559d34115b8f84e3b6178ba480e9d3cdc5
gcastellan0s <g@gustavo-castellanos.com>
2020-01-21 13:56:21 -0600

    Cambios

--- mirari/STR/views.py
+++ mirari/STR/views.py
@@ -10,3 +10,7 @@ from .vars import *
 class InventoryOrder__ListView(Generic__ListView):
     template_name = 'InventoryOrder__ListView.html'
     model = InventoryOrder
+
+class InventoryOrder__CreateView(Generic__CreateView):
+    template_name = 'InventoryOrder__CreateView.html'
+    model = InventoryOrder
