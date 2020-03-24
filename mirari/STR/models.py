342c8633a87950f5b76ba27281c1365e88f13ee0
gcastellan0s <g@gustavo-castellanos.com>
2020-03-24 09:28:21 -0600

    Cambios

--- mirari/STR/models.py
+++ mirari/STR/models.py
@@ -419,7 +419,7 @@ class Product(Model_base):
     def __str__(self):
         return self.name
     def url_list(self):
-        return 'api/InventoryApi/STR/Product/'
+        return '/api/InventoryApi/STR/Product/'
     def get_category(self):
         return self.render_if(self.category.name)
     def get_uid(self):
