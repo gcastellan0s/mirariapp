42116cef0d4ca391d096df0ce016852b32fd9412
gcastellan0s <g@gustavo-castellanos.com>
2020-04-15 23:28:47 -0500

    cambios

--- mirari/STR/views.py
+++ mirari/STR/views.py
@@ -26,18 +26,18 @@ class Inventory__ApiView(Generic__ApiView):
 	@method_decorator(csrf_exempt)
 	def get_serializers(self, request):
 		if request.POST.get('sendData'):
-			#data = json.loads(request.POST.get('sendData'))
-			#inventoryOrder = InventoryOrder()
-			#if data.provider:
-				#inventoryOrder.provider = Provider.objects.get(pk=data.provider)
-			#if data.responsible:
-				#inventoryOrder.responsible = User.objects.get(pk=data.responsible)
-			#inventoryOrder.notes = data.notes
-			#inventoryOrder.operationType = data.type
-			#inventoryOrder.save()
+			data = json.loads(request.POST.get('sendData'))
+			inventoryOrder = InventoryOrder()
+			if data.provider:
+				inventoryOrder.provider = Provider.objects.get(pk=data.provider)
+			if data.responsible:
+				inventoryOrder.responsible = User.objects.get(pk=data.responsible)
+			inventoryOrder.notes = data.notes
+			inventoryOrder.operationType = data.type
+			inventoryOrder.save()
 			#for product in data['productList']:
-			#return JsonResponse({'sendData':inventoryOrder.pk}, safe=False)
-			return JsonResponse({'sendData':request.POST.get('sendData')}, safe=False)
+			return JsonResponse({'sendData':inventoryOrder.pk}, safe=False)
+			#return JsonResponse({'sendData':request.POST.get('sendData')}, safe=False)
 		if request.POST.get('codebar'):
 			return JsonResponse({'product':ProductSerializer(Product.objects.filter(codebar=request.POST.get('codebar')).first()).data}, safe=False)
 		if request.POST.get('productID'):
