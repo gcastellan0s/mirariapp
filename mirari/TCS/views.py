81e8b3837b1c935b05c93dcb799a247e8e56010d
gcastellan0s <g@gustavo-castellanos.com>
2020-04-29 16:50:50 -0500

    Cambios

--- mirari/TCS/views.py
+++ mirari/TCS/views.py
@@ -127,4 +127,59 @@ class TCSapi__ApiView(Generic__ApiView):
     @method_decorator(csrf_exempt)
     def get_serializers(self, request):
         if request.POST.get('getOrders'):
-            return JsonResponse({'ok':'ok'}, safe=False)
+            class my_plugin(Plugin):
+                def egress(self, envelope, http_headers, operation, binding_options):	
+                    xmlString = tostring(envelope, encoding='unicode')	
+                    xmlString = xmlString.replace("&lt;", "<")	
+                    xmlString = xmlString.replace("&gt;", ">")	
+                    xmlString = xmlString.replace("&amp;", "&")	
+                    parser = XMLParser(strip_cdata=False)	
+                    newenvelope = XML(xmlString, parser=parser)	
+                    return newenvelope, http_headers	
+            nscus = 'http://siebel.com/CustomUI'	
+            archive = Element('{%s}LVP_spcConsulta_spcSR_Input' % nscus, nsmap={'cus': nscus})	
+            subelement = SubElement(archive, '{%s}strUsuario' % nscus)	
+            subelement.text = 'PROVEEDOR24'	
+            subelement = SubElement(archive, '{%s}strFechaInicio' % nscus)	
+            subelement.text = ''	
+            request_data = {	
+                'operacion': 'CSR',	
+                'archivo': '<![CDATA['+tostring(archive).decode("utf-8")+']]>',	
+            }	
+            client = Client('http://srproveedores.liverpool.com.mx/wbi/AltaTicket?wsdl', plugins=[my_plugin()])	
+            node = client.create_message(client.service, 'AltaOperation', **request_data)	
+            with open('./xml/TCS/CSR.xml', 'wb') as f:	
+                f.write(tostring(node, pretty_print=True))	
+            headers = {	
+                'Content-Type': 'text/xml',	
+                'SOAPAction': '"http://www.example.org/wbi/AltaOperation"',	
+            }	
+            data = open('./xml/TCS/CSR.xml', 'rb').read()	
+            response = requests.post('https://srproveedoresqa.liverpool.com.mx/wbi/AltaTicket', headers=headers, data=data)	
+            orders = [] 	
+            if (str(response) == '<Response [200]>'):	
+                xml = xmltodict.parse(xmltodict.parse(response.text.encode())['soapenv:Envelope']['soapenv:Body']['NS1:TransferenciaResponse']['NS1:archivo'])	
+                xml = xml['ns:LVP_spcConsulta_spcSR_Output']['ListOfLvpServiceRequestIo']['LvpServiceRequestCustom']	
+                for order in xml:	
+                    if not OrderService.objects.filter(serialLiverpool = order['SRId']):	
+                        orderService = OrderService()	
+                        orderService.serialLiverpool = order['SRId']	
+                        orderService.organization = Organization.objects.filter(pk=3).first()	
+                        orderService.user = User.objects.filter(pk=656).first()	
+                        orderService.delivery_date = datetime.datetime.strptime(order['ShipDate'], "%d/%m/%Y").date()	
+                        orderService.client_name = order['ContactFirstName'] + ' ' + order['ContactLastName']	
+                        orderService.email = order['LVPContacteMail']	
+                        orderService.contact_phone1 = order['LVPContactPhone']	
+                        orderService.address = order['LVPStreetAddress']	
+                        orderService.colony = order['LVPProvince']	
+                        orderService.city = order['LVPCity'] + ' ' + order['LVPState']	
+                        orderService.cp = order['LVPPostalCode']	
+                        orderService.client_notes = order['Abstract'] + ' Area:' + order['INSArea'] + ' SubArea:' + order['INSSub-Area'] + ' Tipo:' + order['LVPType4thLevel']	
+                        orderService.company = Company.objects.filter(pk=37).first()	
+                        orderService.companyName = 'liverpool'	
+                        orderService.report_name = order['LVPCreatedBy']	
+                        orderService.serial_number = order['SerialNumber']	
+                        orderService.order_notes = 'Modelo: ' + order['LVPAssetModel']	
+                        orderService.save()	
+                    orders.append(order['SRId'])	
+            return JsonResponse({'OrderServices':OrderServiceSerializer(OrderService.objects.filter(serialLiverpool__in=orders), many=True).data}, safe=False)
